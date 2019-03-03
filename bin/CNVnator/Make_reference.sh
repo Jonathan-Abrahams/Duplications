4436f4b38214f360bd2aee89f301e28f2721e7c9
Jonathan Abrahams <jsja20@bath.ac.uk>
2019-03-03 15:42:44 +0000

    We are getting there with the reference making

--- bin/CNVnator/Make_reference.sh
+++ bin/CNVnator/Make_reference.sh
@@ -3,11 +3,34 @@
 
 
 #What folder do the other files want?
-mkdir ./Reference_genomes
+mkdir Reference_genomes
 
 #Copy the reference genome there
 
-cp $1 ./Reference/genomes/
+cp $1 ./Reference_genomes/
+#sed -i 's/ /_/g' ./Reference_genomes/$1
+sed -i 's/,//g' ./Reference_genomes/$1
+#split files
+cd Reference_genomes
+cat $1| awk '{
+        if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}
+        print $0 > filename
+}'
+
+cd ..
+rm ./Reference_genomes/$1
 
 #Call the script which will split and rename it
-./bin/CNVnator/file_splitter.sh
+
+#This is troublesome.
+#find ./Reference_genomes/* -print0 |xargs -0 -n1  bash ./bin/CNVnator/file_splitter.sh 
+find ./Reference_genomes/* -print0 -exec bash ./bin/CNVnator/file_splitter.sh "{}" \;
+
+#files=$(find ./Reference_genomes/* -print0)|sed 's/\./\\n\./g'
+#echo $files
+
+#for OUTPUT in "$files"
+#do#
+#	echo $OUTPUT
+#
+#done
--- bin/CNVnator/file_splitter.sh
+++ bin/CNVnator/file_splitter.sh
@@ -1,25 +1,49 @@
 #We will take a single fasta file and  rename the file  to the first word of the header and then rename the fiels as first word+.fa
 
 
-#split files
-#cat $1|awk '{
-#        if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}
-#        print $0 > filename
-#}'
+#echo dollar1 is $1
+#file_name=$(sed  's/ /\\ /g'<<<$1)
+#echo file_name is $file_name
+#cd ./Reference_genomes
+head -1 "$1"
+#head -1 "$file_name"
+#Is_fasta=$(head -1 "$(echo $file_name)"|grep -o ">"|wc -c)
+#echo $Is_fasta
+##(head -1 $1|grep -o ">"
+#if [ "$Is_fasta" -ge "2" ]
+#then
+#	echo "Input is a .fasta file, executing script"
+##Header
+#Header=$(head -1 $file_name|cut -d' ' -f1)
+#echo Header is: $Header
 
-
-#Header
-Header=$(head -1 $1|cut -d' ' -f1)
-echo $Header
 
 
 #Filename
 
-sans_less=$(sed 's/>//g' <<<$Header)
-Filename=$(echo $sans_less.fa)
+#sans_less=$(sed 's/>//g' <<<$Header)
+#Filename=$(echo $sans_less.fa)
 
 
-mv $1 ./$Filename
+#mv file_name ./$Filename
 
-sed -i "1s/.*/$Header/" $Filename
+#sed -i "1s/.*/$Header/" $Filename
+
+#else
+#	echo "File is not a fasta. Doing nothing."
+#fi
+#Header
+#Header=$(head -1 $1|cut -d' ' -f1)
+#echo $Header
+
+
+#Filename
+
+#sans_less=$(sed 's/>//g' <<<$Header)
+#Filename=$(echo $sans_less.fa)
+
+
+#cp $1 ./$Filename
+
+#sed -i "1s/.*/$Header/" $Filename
 
