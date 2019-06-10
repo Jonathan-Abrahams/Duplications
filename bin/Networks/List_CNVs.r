089fa8bf2c6d6ef403926444cb15927639363157
Jonathan Abrahams <jsja20@bath.ac.uk>
2019-06-10 12:33:13 +0000

    Big changes to List_CNVs. Previous script overlooked potentially 10-20% of total CNVs.

--- bin/Networks/List_CNVs.r
+++ bin/Networks/List_CNVs.r
@@ -20,54 +20,52 @@ temp_col=list()
 col_list=vector()
 count=1
 for(i in c(1:ncol(data)))
-{#find the breakpoints
-  #print(i)
+{
+  #find the breakpoints
+
   diffs <- data[,i][-1L] != data[,i][-length(data[,i])]
-  idx <- c(which(diffs), length(data[,i]))
-  # Starts is the gene precding the start of CNV so do a +1
-  starts=idx[as.logical(c(1:length(idx)%%2))]
-  ends=idx[!as.logical(c(1:length(idx)%%2))]
-  starts=starts+1
-  # Remove the final gene as a possible start
-  if(is.na(table(starts%in%c(nrow(data)+1))['TRUE'])!='TRUE')
-  {
-    starts=starts[-which(starts%in%c(nrow(data)+1))]
-  }
+  idx <- c(1,which(diffs), length(data[,i]))
+
   
+  #For every element of IDX, which is every time the number changes
   
+
+    #establish which IDC encodes a change that is 2
+    #
+  duppy_start=idx[which(data[idx,i]>=1.1)-1]+1
+  duppy_end=idx[which(data[idx,i]>=1.1)]
   
-  # 
-  interval=ends-starts
-  # Which starts are 
-  duppy_start=starts[which(data[starts,i]>=1.1)]
-  if(length(duppy_start)>0)
-  {
-    intervals=interval[which(starts%in%duppy_start)]
-    Column_list=list()
-    for(k in c(1:length(duppy_start)))
-    {
-      Column_list[k]=list(duppy_start[k]:c(duppy_start[k]+intervals[k]))
-      copy_number[k]=mean(data[,i][unlist(Column_list[k])])
-      start_temp[k]=duppy_start[k]
-      end_temp[k]=duppy_start[k]+intervals[k]
-      col_list[count]=i
-      count=count+1
+    if(length(duppy_start)>0)
+      {
+      Column_list=list()
       
-    }
-    real_list[i]=list(Column_list)
-    copy_result[i]=list(copy_number)
-    start_list[i]=list(start_temp)
-    end_list[i]=list(end_temp)
-    copy_number=list()
-    Column_list=list()
-    start_temp=list()
-    end_temp=list()
-    lenny_l[i]=length(real_list[[i]])
+        for(k in c(1:length(duppy_start)))
+        {
+          
+          intervals=duppy_start[k]:duppy_end[k]
+          
+          Column_list[k]=list(duppy_start[k]:duppy_end[k])
+          copy_number[k]=mean(data[,i][unlist(Column_list[k])])
+          start_temp[k]=duppy_start[k]
+          end_temp[k]=duppy_end[k]
+          col_list[count]=i
+          count=count+1
+
+        }
+        real_list[i]=list(Column_list)
+        copy_result[i]=list(copy_number)
+        start_list[i]=list(start_temp)
+        end_list[i]=list(end_temp)
+        copy_number=list()
+        Column_list=list()
+        start_temp=list()
+        end_temp=list()
+        lenny_l[i]=length(real_list[[i]])
     
+    }
   }
-  
-  
-}
+
+ 
 
 result_frame=data.frame(Starts=unlist(start_list),Ends=unlist(end_list),
                         Length=c(unlist(end_list)-unlist(start_list)),
