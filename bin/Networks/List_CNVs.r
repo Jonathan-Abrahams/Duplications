0b7d52feb62c0a05f12d918b599b8746fa2cd0be
Jonathan Abrahams <jsja20@bath.ac.uk>
2019-06-12 11:25:24 +0000

    Fixed List_CNV

--- bin/Networks/List_CNVs.r
+++ bin/Networks/List_CNVs.r
@@ -25,49 +25,45 @@ for(i in c(1:ncol(data)))
 
   diffs <- data[,i][-1L] != data[,i][-length(data[,i])]
   idx <- c(1,which(diffs), length(data[,i]))
-
+  count=1
   
-  #For every element of IDX, which is every time the number changes
-  
-
-    #establish which IDC encodes a change that is 2
-    #
-  duppy_start=idx[which(data[idx,i]>=1.1)-1]+1
-  duppy_end=idx[which(data[idx,i]>=1.1)]
-  
-    if(length(duppy_start)>0)
-      {
-      Column_list=list()
-      
-        for(k in c(1:length(duppy_start)))
+  copy_number=vector()
+  start_temp=vector()
+  end_temp=vector()
+  col_list=vector()
+        for(k in c(1:c(length(idx)-1)))
         {
-          
-          intervals=duppy_start[k]:duppy_end[k]
-          
-          Column_list[k]=list(duppy_start[k]:duppy_end[k])
-          copy_number[k]=mean(data[,i][unlist(Column_list[k])])
-          start_temp[k]=duppy_start[k]
-          end_temp[k]=duppy_end[k]
+          #First is +1 OUTSIDE of brackets, second is +1 inside brackets
+          intervals=c(idx[k]+1):idx[k+1]
+          if(as.numeric(names(table(data[intervals,i])[1]))>1)
+          {
+                      
+          #Column_list[k]=list(idx[k]:idx[k+1])
+          copy_number[count]=mean(data[intervals,i])
+          start_temp[count]=idx[k]+1
+          end_temp[count]=idx[k+1]
           col_list[count]=i
           count=count+1
 
+          }
         }
-        real_list[i]=list(Column_list)
+  
+        real_list[i]=list(col_list)
         copy_result[i]=list(copy_number)
         start_list[i]=list(start_temp)
         end_list[i]=list(end_temp)
         copy_number=list()
-        Column_list=list()
+       # Column_list=list()
         start_temp=list()
         end_temp=list()
-        lenny_l[i]=length(real_list[[i]])
+        #lenny_l[i]=length(real_list[[i]])
     
     }
-  }
+  
 
  
 
 result_frame=data.frame(Starts=unlist(start_list),Ends=unlist(end_list),
                         Length=c(unlist(end_list)-unlist(start_list)),
-                        Copy_number=unlist(copy_result),Strains=colnames(data)[col_list])
+                        Copy_number=unlist(copy_result),Strains=colnames(data)[unlist(real_list)])
 write.csv(result_frame,args[2])
